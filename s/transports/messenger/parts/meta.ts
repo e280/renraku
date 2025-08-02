6741cd3b1d00fff6d2c743f050edfb69c99f93e5
Chase Moskal <chasemoskal@gmail.com>
2025-08-01 20:26:28 -0700

    export: MessengerMeta

--- s/transports/messenger/parts/meta.ts
+++ s/transports/messenger/parts/meta.ts
@@ -3,7 +3,7 @@ import {Fns} from "../../../core/types.js"
 import {Remote} from "../../../core/remote-proxy.js"
 
 export class MessengerMeta<F extends Fns> {
-	transfer: Transferable[] | undefined = undefined
+	transfer: Transferable[] | undefined
 
 	constructor(public remote: Remote<F>) {}
 }
